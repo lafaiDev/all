801040c2f486abad0fbf9b2266446ab7d0828a1c
lafaiDev <ahmed.lafai@gmail.com>
2016-01-12 09:23:24 +0000

    first commit

---
+++ module/Album/config/module.config.php
@@ -0,0 +1,88 @@
+<?php
+// http://stackoverflow.com/questions/13007477/doctrine-2-and-zf2-integration
+namespace Album; // SUPER important for Doctrine othervise can not find the Entities
+
+return array(
+    'controllers' => array(
+        'invokables' => array(
+            'Album\Controller\Index' => 'Album\Controller\IndexController',
+        ),
+    ),
+    'router' => array(
+        'routes' => array(
+            'album' => array(
+                'type'    => 'Literal',
+                'options' => array(
+                    'route'    => '/album',
+                    'defaults' => array(
+                        '__NAMESPACE__' => 'Album\Controller',
+                        'controller'    => 'Index',
+                        'action'        => 'index',
+                    ),
+                ),
+                'may_terminate' => true,
+                'child_routes' => array(
+                    'default' => array(
+                        'type'    => 'Segment',
+                        'options' => array(
+                            'route'    => '/[:controller[/:action[/:id]]]',
+                            'constraints' => array(
+                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'id'     	 => '[0-9]*',
+                            ),
+                            'defaults' => array(
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        ),
+    ),
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'album' => __DIR__ . '/../view'
+        ),
+        'strategies'         => array('ViewJsonStrategy'),
+        'display_exceptions' => true,
+    ),
+
+    // extra for Doctrine Put the namespace on top!!!!!!!
+    // http://stackoverflow.com/questions/13007477/doctrine-2-and-zf2-integration
+    // put namespace User; in the first line of your module.config.php. a namespace should be defined as you use the __NAMESPACE__ constant...
+    // from DoctrineModule
+    'doctrine' => array(
+
+
+        // 2) standard configuration for the ORM from https://github.com/doctrine/DoctrineORMModule
+        // http://www.jasongrimes.org/2012/01/using-doctrine-2-in-zend-framework-2/
+        // ONLY THIS IS REQUIRED IF YOU USE Doctrine in the module
+        'driver' => array(
+            // defines an annotation driver with two paths, and names it `my_annotation_driver`
+//            'my_annotation_driver' => array(
+            __NAMESPACE__ . '_driver' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'cache' => 'array',
+                'paths' => array(
+                    // __DIR__ . '/../module/AuthDoctrin/src/AuthDoctrine/Entity' // 'path/to/my/entities',
+                    __DIR__ . '/../src/' . __NAMESPACE__ . '/Entity',
+                    // 'H:\PortableApps\PortableGit\projects\btk\module\Auth\src\Auth\Entity' // Stoyan added to use doctrine in Auth module
+//-					__DIR__ . '/../../Auth/src/Auth/Entity', // Stoyan added to use doctrine in Auth module
+                    // 'another/path'
+                ),
+            ),
+
+            // default metadata driver, aggregates all other drivers into a single one.
+            // Override `orm_default` only if you know what you're doing
+            'orm_default' => array(
+                'drivers' => array(
+                    // register `my_annotation_driver` for any entity under namespace `My\Namespace`
+                    // 'My\Namespace' => 'my_annotation_driver'
+                    // 'AuthDoctrine' => 'my_annotation_driver'
+                    __NAMESPACE__ . '\Entity' => __NAMESPACE__ . '_driver',
+//-					'Auth\Entity' => __NAMESPACE__ . '_driver' // Stoyan added to allow the module Auth also to use Doctrine
+                )
+            )
+        )
+    ),
+);
---
+++ module/Album/src/Album/Controller/IndexController.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace Album\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+use Album\Entity\Album;
+
+use DoctrineModule\Stdlib\Hydrator\DoctrineObject as DoctrineHydrator;
+use DoctrineORMModule\Stdlib\Hydrator\DoctrineEntity;
+use DoctrineORMModule\Form\Annotation\AnnotationBuilder as DoctrineAnnotationBuilder;
+
+// for the form
+use Zend\Form\Element;
+
+use Zend\View\Model\JSonModel;
+
+class IndexController extends AbstractActionController
+{
+
+    // Retrieve - Albums
+    public function indexAction()
+    {
+        $entityManager = $this->getServiceLocator()->get('doctrine.entitymanager.orm_default');
+        $albums = $entityManager->getRepository('Album\Entity\Album')->findAll();
+        return new ViewModel(array('rowset' => $albums));
+    }
+
+    // Create - C
+    public function createAction()
+    {
+        $entityManager = $this->getServiceLocator()->get('doctrine.entitymanager.orm_default');
+        $album = new Album;
+        $builder = new DoctrineAnnotationBuilder($entityManager);
+        $form = $builder->createForm( $album );
+        $form->setHydrator(new DoctrineHydrator($entityManager,'Album\Entity\Album'));
+
+        $send = new Element('submit');
+        $send->setValue('Go'); // submit
+        $send->setAttributes(array(
+            'type'  => 'submit'
+        ));
+        $form->add($send);
+
+        $form->bind($album);
+
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $form->setData($request->getPost());
+            if ($form->isValid()) {
+                $entityManager->persist($album);
+                $entityManager->flush();
+
+                return $this->redirect()->toRoute('album/default', array('controller' => 'index', 'action' => 'index'));
+            }
+        }
+
+        return new ViewModel(array('form' => $form));
+    }
+
+    // Update - U
+    public function updateAction()
+    {
+        $id = $this->params()->fromRoute('id');
+        if (!$id) return $this->redirect()->toRoute('album/default', array('controller' => 'index', 'action' => 'index'));
+
+        $entityManager = $this->getServiceLocator()->get('doctrine.entitymanager.orm_default');
+
+        try {
+            $repository = $entityManager->getRepository('Album\Entity\Album');
+            $album = $repository->find($id);
+
+        }
+        catch (\Exception $ex) {
+            echo $ex->getMessage(); // this never will be seen if you don't comment the redirect
+            return $this->redirect()->toRoute('album/default', array('controller' => 'index', 'action' => 'index'));
+        }
+
+        $builder = new DoctrineAnnotationBuilder($entityManager);
+        $form = $builder->createForm( $album );
+        $form->setHydrator(new DoctrineHydrator($entityManager,'Album\Entity\Album'));
+
+        $send = new Element('submit');
+        $send->setValue('Go'); // submit
+        $send->setAttributes(array(
+            'type'  => 'submit'
+        ));
+        $form->add($send);
+
+        $form->bind($album);
+
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $form->setData($request->getPost());
+            if ($form->isValid()) {
+                $entityManager->persist($album);
+                $entityManager->flush();
+
+                return $this->redirect()->toRoute('album/default', array('controller' => 'index', 'action' => 'index'));
+            }
+        }
+
+        return new ViewModel(array('form' => $form, 'id' => $id));
+    }
+
+    // Delete - D
+    public function deleteAction(){
+
+		$id = $this->params()->fromRoute('id');
+		if (!$id) return $this->redirect()->toRoute('album/default', array('controller' => 'index', 'action' => 'index'));
+
+		$entityManager = $this->getServiceLocator()->get('doctrine.entitymanager.orm_default');
+
+        try {
+			$repository = $entityManager->getRepository('Album\Entity\Album');
+			$album = $repository->find($id);
+			$entityManager->remove($album);
+			$entityManager->flush();
+        }
+        catch (\Exception $ex) {
+         // ... maybe do something more
+             return $this->redirect()->toRoute('album/default', array('controller' => 'index', 'action' => 'index'));
+        }
+
+		return $this->redirect()->toRoute('album/default', array('controller' => 'index', 'action' => 'index'));
+	}
+    public function deleteajaxAction(){
+        $id  = (int) $_POST['id'];
+        $entityManager = $this->getServiceLocator()->get('doctrine.entitymanager.orm_default');
+        try {
+            $repository = $entityManager->getRepository('Album\Entity\Album');
+            $album = $repository->find($id);
+            $entityManager->remove($album);
+            $entityManager->flush();
+            $msg = "the selected album was successfuly deleted";
+        }
+        catch (\Exception $ex) {
+            // ... maybe do something more
+            $msg=$ex->getMessage();
+        }
+
+
+
+        $response = new JSonModel(array('response' => $msg));
+        return $response;
+    }
+}
